733d34e271d03ea9023fc131c627dfef3c817563
Vannou28 <vannier.aurelien2@gmail.com>
2022-12-01 13:27:17 +0100

    creation de formulaitres

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { User } from './User';
+
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+    user:User = new User("","","","")
+
+    /*submitted: boolean;
+    valid: boolean;
+    
+    constructor(){
+        this.submitted = false;
+        this.valid = false;
+    }*/
+
+
+    onSubmit(){
+        console.log(this.user);
+       
+
+    }
+
+}
